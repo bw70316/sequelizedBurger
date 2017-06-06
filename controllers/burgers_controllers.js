3aa758e33ce6f6b35b396a833def3ca18076f90d
Your Name <you@example.com>
2017-06-05 22:47:18 -0400

    sequelize not working

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -3,42 +3,41 @@ var express = require("express");
 var router = express.Router();
 
 // Import the model (cat.js) to use its database functions.
-var burger = require("../models/burger.js");
+var db = require("../models");
 
 // Create all our routes and set up logic within those routes where required.
 //Each route works with the ORM.js to perform the specific functions. 
 router.get("/", function(req, res) {
-  burger.selectAll(function(data) {
+  db.Burger.findAll({}).then(function(data) {
     var hbsObject = {
       burgers: data
-    };
-    console.log(hbsObject);
+    };    
     res.render("index", hbsObject);
-  });
+  })
 });
 
 router.post("/", function(req, res) {
-  burger.insertOne([
-    "burger_name"
-  ], [
-    req.body.burger_name
-  ], function() {
-    res.redirect("/");
+  db.Burger.create({
+    burger_name: req.body.burger_name
+  }).then(function() {
+        res.redirect("/");
   });
 });
+
+//describes where we want to update, in this case the devoured boolean from false to true 
 
 router.put("/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
-
-  console.log("condition", condition);
-
-  burger.updateOne({
-    devoured: true
-  }, condition, function() {
+  db.Burger.update({
+      devoured: true
+  }, {
+      where: {
+      id: req.params.id
+  }
+  }).then(function() {
     res.redirect("/");
   });
 });
-//when a burger is devoured, the page updates to display the burger matched with the id
+
 
 // Export routes for server.js to use.
 module.exports = router;
--- db/seeds.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO burgers (burger_name) VALUES ('Bacon Cheeseburger');
-INSERT INTO burgers (burger_name) VALUES('Mushroom Swiss Burger');
-INSERT INTO burgers (burger_name) VALUES('Carolina Burger');
